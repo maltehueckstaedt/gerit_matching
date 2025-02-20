b4e539d7383310a7ee23150d64e29011565f361b
Malte Hückstädt <47598771+maltehueckstaedt@users.noreply.github.com>
2025-02-20 16:40:54 +0100

    Create hex_match_prep.r

---
+++ hex_match_prep.r
@@ -0,0 +1,13 @@
+library(hexmatch)
+find_names()
+
+setup_matching(name_gerit = "Heinrich-Heine-Universität Düsseldorf", 
+               name_folder = "Heinrich-Heine-Universitaet_Duesseldorf", 
+               name_rds = "data_heinrich_heine_universitaet_duesseldorf.rds" ,
+               with_organisation_alternativ = FALSE)
+
+# voraussetzung: data_heinrich_heine_universitaet_duesseldorf.rds im enviorment.
+# Wichtige Variablen: df_gerit: Variable: Einrichtung=Gerit kleinste Orga-Einheit
+# df_scraped: organisation: Orga-Namen, die aus dem ScrAPING kommen. Gemachte wete in "gerit_organisation" schreiben
+# In df_scraped: organisation_type_for_matching "combinations" rausfiltern. 
+#nach dem Matching: is_luf_unique: rausfiltern.
